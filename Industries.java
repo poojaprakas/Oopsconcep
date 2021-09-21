8042c07af080a8be96611b678e79cf35e9eacc9c
poojaprakas <89781163+poojaprakas@users.noreply.github.com>
2021-09-21 07:23:17 -0700

    Add files via upload

---
+++ Industries.java
@@ -0,0 +1,10 @@
+package com.xworkz.oopConcepts;
+
+public class Industries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Magzine.java
@@ -0,0 +1,24 @@
+package com.xworkz.oopConcepts;
+
+public class Magzine {
+	int images;
+	int articles;
+	int pages;
+	String distribution_place;
+	String articlename;
+	public void startPrint() {
+		System.out.println("start to print the new article");
+	}
+	public void distribution() {
+		System.out.println("articles distributed to the corect place");
+	}
+	public void copies() {
+		System.out.println("every copies design with new stories");
+	}
+	public void stopPrint() {
+
+		System.out.println("printing stop at the right time");
+
+	}
+
+}
